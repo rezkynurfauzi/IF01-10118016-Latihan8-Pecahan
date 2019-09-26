3f038894c78b6d9478dfc420fd56d77e4fc9b3b4
rezkynurfauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-09-26 12:36:52 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan8-Pecahan/src/pecahan/Pecahan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pecahan;
+
+/**
+ *
+ * Nama : Rezky Nur Fauzi
+ * NIM  : 10118016  
+ * Kelas: IF-1 
+ * Deskripsi Program : Menampilkan Pecahan pada Java
+ */
+public class Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float f;
+        double d;
+        
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
